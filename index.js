42c2f13a682946d486dc89e17b9cb7c6bba7b263
Alejandro Vivanco <manuelinviv@gmail.com>
2021-02-20 17:45:24 -0500

    finalizado primer crud de entidades

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express');
+const queries = require('./lib/actions/participantes');
 const cors = require('cors');
+var bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 const { PORT } = process.env;
 
 const app = express();
+//bodyParser permite habilitar el envio de informacion por body ya sea urlencoded o json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+//Home
+app.get('/', (req, res) => {
+  res.send('Bienvenido');
 });
 
+/*-------------------Participantes-----------------*/
+//GetAll
+app.get('/participantes', async (req, res) => {
+  res.send(await queries.getParticipantes());
+});
+//GetOne
+app.get('/participantes/:dni', async (req, res) => {
+  res.send(await queries.getParticipante(req.params.dni));
+});
+//Post
+app.post('/participantes', async (req, res) => {
+  res.send(await queries.createParticipantes(req.body));
+});
+//Update
+app.put('/participantes/:dni', async (req, res) => {
+  res.send(await queries.updateParticipante(req.params.dni, req.body));
+});
+//Delete
+app.delete('/participantes/:dni', async (req, res) => {
+  res.send(await queries.deleteParticipante(req.params.dni));
+});
+
+//Instancia del servidor
 app.listen(PORT, () => {
   console.log(`app listening at port ${PORT}`);
 });
