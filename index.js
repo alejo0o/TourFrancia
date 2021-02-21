402da0878f7289766dcd24487451a6ac5c5134d4
AngeloBenavidez <benavidezangelo9@gmail.com>
2021-02-21 12:27:47 -0500

    Crud de ganadores

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const queries = require('./lib/actions/participantes');
+const queries = require('./lib/actions/ganadores');
 const cors = require('cors');
 var bodyParser = require('body-parser');
 
@@ -38,6 +39,29 @@ app.put('/participantes/:dni', async (req, res) => {
 app.delete('/participantes/:dni', async (req, res) => {
   res.send(await queries.deleteParticipante(req.params.dni));
 });
+
+/*-------------------Ganadores-----------------*/
+//GetAll
+app.get('/ganadores', async (req, res) => {
+  res.send(await queries.getGanadores());
+});
+//GetOne
+app.get('/ganadores/:dni', async (req, res) => {
+  res.send(await queries.getGanador(req.params.dni));
+});
+//Post
+app.post('/ganadores', async (req, res) => {
+  res.send(await queries.createGanadores(req.body));
+});
+//Update
+app.put('/ganadores/:dni', async (req, res) => {
+  res.send(await queries.updateGanador(req.params.dni, req.body));
+});
+//Delete
+app.delete('/ganadores/:dni', async (req, res) => {
+  res.send(await queries.deleteGanador(req.params.dni));
+});
+
 
 //Instancia del servidor
 app.listen(PORT, () => {
